6d29b2128ce4766e57aafc382a8f4ee236de8fdf
Pepper-Bell <106971788+Pepper-Bell@users.noreply.github.com>
2022-06-12 16:34:42 +0900

    Update symptom.c

--- symptom.c
+++ symptom.c
@@ -3,32 +3,32 @@
 #include<string.h>
 
 void find(void) {
-	int num0; int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7; int *sid[7] = { &a, &b, &c, &d, &e, &f, &g };
-	printf("\n���� �ް� �ִ� ������ �����Ͻÿ�\n->");
-	printf("\n1.��ħ�� ����ϴ�.\n2.�Ƿΰ��� ���� �����ϴ�.\n3.������ �� �ðų� �̰��� ����� �ֽ��ϴ�.\n4.������ �ֽ��ϴ�.\n5.���簡 �ֽ��ϴ�.\n6.ȣ������� �޽��ϴ�.\n7.����� ����� �ֽ��ϴ�.\n->");
+	int num0; int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7; int *sid[7] = { &a, &b, &c, &d, &e, &f, &g };  //포인터배열_if문에서 입력 숫자와 비교위한 정수
+	printf("\n현재 겪고 있는 증상을 선택하시오\n->");
+	printf("\n1.기침이 잦습니다.\n2.피로감을 쉽게 느낍니다.\n3.냄새를 못 맡거나 미각의 상실이 있습니다.\n4.두통이 있습니다.\n5.설사가 있습니다.\n6.호흡곤란을 겪습니다.\n7.언어의 상실이 있습니다.\n->");
 	scanf_s("%d", &num0);
 	if (num0==*sid[0]) {
-		printf("\n��ħ�� �ڷγ�-19�� ���� ���� �����Դϴ�\n");
+		printf("\n기침은 코로나-19의 가장 흔한 증상입니다\n");
 	}
 	else if (num0 ==*sid[1]) {
-		printf("\n�Ƿδ� �ڷγ�-19�� ���� ���� �����Դϴ�\n");
+		printf("\n피로는 코로나-19의 가장 흔한 증상입니다\n");
 	}
 	else if (num0 == *sid[2]) {
-		printf("\n�İ��� �̰��� ����� �ڷγ�-19�� ���� ���� �����Դϴ�\n");
+		printf("\n후각과 미각의 상실은 코로나-19의 가장 흔한 증상입니다\n");
 	}
 	else if (num0 == *sid[3]) {
-		printf("\n������ �ڷγ�-19�� �� ���� �����Դϴ�\n");
+		printf("\n두통은 코로나-19의 덜 흔한 증상입니다\n");
 	}
 	else if (num0 == *sid[4]) {
-		printf("\n����� �ڷγ�-19�� �� ���� �����Դϴ�\n");
+		printf("\n설사는 코로나-19의 덜 흔한 증상입니다\n");
 	}
 	else if (num0 == *sid[5]) {
-		printf("\nȣ������� �ڷγ�-19�� �ɰ��� �����Դϴ�\n");
+		printf("\n호흡곤란은 코로나-19의 심각한 증상입니다\n");
 	}
 	else if (num0 == *sid[6]) {
-		printf("\n��� ��ǰ� ���������� �ڷγ�-19�� �ɰ��� �����Դϴ�\n");
+		printf("\n언어 상실과 어지러움은 코로나-19의 심각한 증상입니다\n");
 	}
 	else {
-		printf("\n�������� �ʴ� ��ȣ�Դϴ�.\n");
+		printf("\n지원하지 않는 번호입니다.\n");
 	}
 }
